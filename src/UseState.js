426f9ea5ab117e740c47a28d17b91dcebd21a356
Alessandrovaru <alessandrovaru@gmail.com>
2023-01-30 12:13:05 -0400

    Undestanding effects

--- src/UseState.js
+++ src/UseState.js
@@ -1,14 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const UseState = () => {
-  const [error, setError] = useState(true)
+  const [error, setError] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    console.log('Starting the effect');
+    if (loading) {
+      setTimeout(() => {
+        console.log('Doing the validation');
+        setLoading(false);
+        console.log('Validated')
+      }, 3000);
+    }
+    console.log('End of effect');
+  }, [loading])
+
+  
   return (
     <>
       <h2>Eliminar ClassState</h2>
       {error && <> twm</>}
+      {loading && <> Cargando...</>}
       <p>Por favor, escribe el code de seguridad</p>
       <input placeholder='Codigo de seguridad' />
-      <button onClick={()=>{setError(!error)}}>Try</button>
+      <button onClick={()=>{setLoading(true)}}>Try</button>
     </>
   )
 }
