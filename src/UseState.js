efccc611ee58feda2f536c3a175ff043b4d6164f
Alessandrovaru <alessandrovaru@gmail.com>
2023-01-30 18:30:08 -0400

    Using effects

--- src/UseState.js
+++ src/UseState.js
@@ -1,15 +1,26 @@
 import React, { useEffect, useState } from 'react'
 
+const SECURITY_CODE = '123xxx'
+
 const UseState = () => {
-  const [error, setError] = useState(true);
+  const [value, setValue] = useState('second')
+  const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     console.log('Starting the effect');
     if (loading) {
+      setError(false);
       setTimeout(() => {
         console.log('Doing the validation');
-        setLoading(false);
+        if (value === SECURITY_CODE) {
+          setLoading(false);
+          
+        } else {
+          setLoading(false);
+          setError(true);
+        }
+        
         console.log('Validated')
       }, 3000);
     }
@@ -20,10 +31,16 @@ const UseState = () => {
   return (
     <>
       <h2>Eliminar ClassState</h2>
-      {error && <> twm</>}
+      {error && <> No coincide</>}
       {loading && <> Cargando...</>}
       <p>Por favor, escribe el code de seguridad</p>
-      <input placeholder='Codigo de seguridad' />
+      <input 
+        placeholder='Codigo de seguridad' 
+        value={value} 
+        onChange={(event)=>{
+          setValue(event.target.value);
+        }} 
+      />
       <button onClick={()=>{setLoading(true)}}>Try</button>
     </>
   )
