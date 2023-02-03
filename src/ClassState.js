2329823bc676aa28b5ca4b6eb3222065f25463be
Alessandrovaru <alessandrovaru@gmail.com>
2023-02-02 20:50:29 -0400

    imperative states

--- src/ClassState.js
+++ src/ClassState.js
@@ -11,6 +11,8 @@ class ClassState extends React.Component {
       value: '',
       error: false,
       loading: false,
+      deleted: false,
+      confirmed: false
     };
   }
 
@@ -31,7 +33,11 @@ class ClassState extends React.Component {
         console.log("Haciendo la validación")
   
         if (SECURITY_CODE === this.state.value) {
-          this.setState({ error: false, loading: false });
+          this.setState({ 
+            error: false, 
+            loading: false,
+            confirmed: true,
+          });
         } else {
           this.setState({ error: true, loading: false });
         }
@@ -42,32 +48,73 @@ class ClassState extends React.Component {
   }
   
   render() {
-    return (
-      <div>
-        <h2>Eliminar {this.props.name}</h2>
-        
-        <p>Por favor, escribe el código de seguridad.</p>
-
-        {(this.state.error && !this.state.loading) && (
-          <p>Error: el código es incorrecto</p>
-        )}
-
-        {this.state.loading && (
-          <Loading />
-        )}
-
-        <input
-          placeholder="Código de seguridad"
-          value={this.state.value}
-          onChange={(event) => {
-            this.setState({ value: event.target.value });
-          }}
-        />
-        <button
-          onClick={() => this.setState({ loading: true })}
-        >Comprobar</button>
-      </div>
-    );
+    if (!this.state.deleted && !this.state.confirmed) {
+      return (
+        <div>
+          <h2>Eliminar {this.props.name}</h2>
+          
+          <p>Por favor, escribe el código de seguridad.</p>
+  
+          {(this.state.error && !this.state.loading) && (
+            <p>Error: el código es incorrecto</p>
+          )}
+  
+          {this.state.loading && (
+            <Loading />
+          )}
+  
+          <input
+            placeholder="Código de seguridad"
+            value={this.state.value}
+            onChange={(event) => {
+              this.setState({ value: event.target.value });
+            }}
+          />
+          <button
+            onClick={() => this.setState({ loading: true })}
+          >Comprobar</button>
+        </div>
+      );
+    } else if (!!this.state.confirmed && !this.state.deleted) {
+      return (
+        <>
+          <p>We ask for a confirmation, are you sure?</p>
+          <button 
+            onClick={() => {
+              this.setState({
+                ...this.state, 
+                deleted: true
+              });
+            }}
+          >Yeah</button>
+          <button 
+            onClick={() => {
+              this.setState({
+                ...this.state, 
+                confirmed: false,
+                value: '',
+              });
+            }}
+          >Nope</button>
+        </>
+      )
+    } else {
+      return(
+        <>
+          <p>eliminado con exito</p>
+          <button 
+            onClick={() => {
+              this.setState({
+                ...this.state, 
+                confirmed: false,
+                deleted: false,
+                value: '',
+              });
+            }}
+          >Reset</button>
+        </>
+      )
+    }
   }
 }
 
