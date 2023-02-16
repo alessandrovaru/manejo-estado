4fdc10dce0e6a85bac8cf2d5c8eadcd69d2e5b2c
Alessandro <alessandrovaru@gmail.com>
2023-02-16 10:47:10 -0400

    UseReducer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 
 import { UseState } from './UseState.js';
 import { ClassState } from './ClassState.js';
+import { UseReducer } from './UseReducer';
 
 function App() {
   return (
     <div className="App">
       <UseState />
       <ClassState />
+      <UseReducer />
     </div>
   );
 }
